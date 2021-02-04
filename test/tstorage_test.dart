342c8ed28c733bc8923a68d3623415e3f9c3d4d5
Shrihari <hari@Shriharis-MacBook-Pro-2.local>
2021-02-04 22:07:30 +0100

    tests added

--- test/tstorage_test.dart
+++ test/tstorage_test.dart
@@ -2,12 +2,37 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tstorage/tstorage.dart';
 
-void main() {
-  // test('adds one to input values', () {
-  //   final calculator = Calculator();
-  //   expect(calculator.addOne(2), 3);
-  //   expect(calculator.addOne(-7), -6);
-  //   expect(calculator.addOne(0), 1);
-  //   expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  // });
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+
+
+void main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+Storage s;
+
+  const channel = MethodChannel('plugins.flutter.io/path_provider');
+  void setUpMockChannels(MethodChannel channel) {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall.method == 'getApplicationDocumentsDirectory') {
+        return '.';
+      }
+    });
+  }
+
+  setUpAll(() async {
+    setUpMockChannels(channel);
+  });
+
+  setUp(() async {
+    s = Storage();
+  });
+
+  test('write, read listen', () async {
+     s.write('test', 'a');
+    final val = await s.val.stream.first;
+    expect('a', val);
+  });
+
 }
+
